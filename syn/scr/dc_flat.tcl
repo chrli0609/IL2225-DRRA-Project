10e0c4d8b914fc91e790abd5c471b8225724cb66
tux.tu@outlook.com <tux.tu@outlook.com>
2023-12-31 20:48:08 +0100

    restore everything to the origin

--- syn/scr/dc_flat.tcl
+++ syn/scr/dc_flat.tcl
@@ -66,17 +66,6 @@ set OUT_DIR ../syn/db;           # output files: netlist, sdf sdc etc.
 set SOURCE_DIR ../rtl;           # rtl code that should be synthesised
 set SYN_DIR ../syn;              # synthesis directory, synthesis scripts constraints etc.
 
-set hierarchy_files [split [read [open ${SOURCE_DIR}/pkg_hierarchy.txt r]] "\n"]
-foreach filename [lrange ${hierarchy_files} 0 end-1] {
-    if {![string equal [string index $filename 0] "#"]} {
-        if {[string equal [file extension $filename] ".vhd"]} {
-            analyze -format vhdl -lib WORK ${SOURCE_DIR}/${filename}
-        } elseif {[string equal [file extension $filename] ".v"]} {
-            analyze -format verilog -lib WORK ${SOURCE_DIR}/${filename}
-        }
-    }
-}
-
 #set hierarchy files and analyze them
 set hierarchy_files [split [read [open ${SOURCE_DIR}/${TOP_NAME}_hierarchy.txt r]] "\n"]
 
