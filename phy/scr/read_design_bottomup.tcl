898507e600c909546569c38ef6f0746fdc47c0a9
tux.tu@outlook.com <tux.tu@outlook.com>
2024-01-01 20:42:59 +0100

    uniquify design before read

--- phy/scr/read_design_bottomup.tcl
+++ phy/scr/read_design_bottomup.tcl
@@ -6,5 +6,6 @@ set_db init_power_nets {VDD}
 set_db init_ground_nets {VSS}
 read_mmmc ${MMMC_FILE}
 read_physical -lef ${LEF_FILE}
+set_db init_design_uniquify true
 read_netlist ${NETLIST_FILE}
 init_design
