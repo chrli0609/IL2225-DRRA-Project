7e62181777b27701a3dd5e0338f282d99031749c
tux.tu@outlook.com <tux.tu@outlook.com>
2024-01-02 12:06:29 +0100

    try floorplan

---
+++ phy/scr/design_variables.tcl
@@ -0,0 +1,26 @@
+set partition_hinst_list {
+	Silago_top_l_corner_inst_0_0 \
+	Silago_bot_l_corner_inst_0_1 \
+	Silago_top_inst_1_0 \
+	Silago_bot_inst_1_1 \
+	Silago_top_inst_2_0 \
+	Silago_bot_inst_2_1 \
+	Silago_top_inst_3_0 \
+	Silago_bot_inst_3_1 \
+	Silago_top_inst_4_0 \
+	Silago_bot_inst_4_1 \
+	Silago_top_inst_5_0 \
+	Silago_bot_inst_5_1 \
+	Silago_top_inst_6_0 \
+	Silago_bot_inst_6_1 \
+	Silago_top_r_corner_inst_7_0 \
+	Silago_bot_r_corner_inst_7_1}
+
+#partition module names from RTL
+set partition_module_list {
+    Silego_top_left_corner \
+    Silego_top \
+    Silego_top_right_corner \
+    Silego_bot_left_corner \
+    Silego_bot \
+    Silego_bot_right_corner}
