4b472eeb1451d4d24c49bc64db4061e63cf7af1e
tux.tu@outlook.com <tux.tu@outlook.com>
2024-01-02 12:40:53 +0100

    try to fix partition.tcl

--- phy/scr/partition.tcl
+++ phy/scr/partition.tcl
@@ -1,11 +1,7 @@
 # create partitions 
 foreach part_hinst ${partition_hinst_list} {
     puts $part_hinst
-    create_partition -hinst ${part_hinst} -core_spacing 0.0 0.0 0.0 0.0 -rail_width 0.0 -min_pitch_left 2 \ 
-                    -min_pitch_right 2 -min_pitch_top 2 -min_pitch_bottom 2 -reserved_layer {1 2 3 4} \
-                     -pin_layer_top {2 4} -pin_layer_left {3} -pin_layer_bottom {2 4} \
-                     -pin_layer_right {3} -place_halo 10.0 0.0 0.0 0.0 -route_halo 10.0 -route_halo_top_layer 7 \ 
-                    -route_halo_bottom_layer 1
+    create_partition -hinst ${part_hinst} -core_spacing 0.0 0.0 0.0 0.0 -rail_width 0.0 -min_pitch_left 2 -min_pitch_right 2 -min_pitch_top 2 -min_pitch_bottom 2 -reserved_layer {1 2 3 4} -pin_layer_top {2 4} -pin_layer_left {3} -pin_layer_bottom {2 4} -pin_layer_right {3} -place_halo 10.0 0.0 0.0 0.0 -route_halo 10.0 -route_halo_top_layer 7 -route_halo_bottom_layer 1
 }
 ##
 align_partition_clones -update_user_grid -pg_horizontal_grid -pg_vertical_grid
