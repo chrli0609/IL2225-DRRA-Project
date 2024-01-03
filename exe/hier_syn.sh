02c28982cc607a9cc8471ca37367ef8bf556230b
tux.tu@outlook.com <tux.tu@outlook.com>
2024-01-03 13:08:31 +0100

    SPC_compile

--- exe/hier_syn.sh
+++ exe/hier_syn.sh
@@ -10,29 +10,29 @@ wait_for_session_exit() {
 # tmux send-keys -t innovus_session 'source ../phy/scr/create_partitions.tcl' Enter
 # tmux send-keys -t innovus_session 'exit' Enter
 
-tmux new-session -d -s innovus_session 'innovus -stylus'
-tmux send-keys -t innovus_session 'cd ../phy/db/part/Silago_top_left_corner.enc.dat/' Enter
-tmux send-keys -t innovus_session 'source ../../../scr/pnr_partition.tcl' Enter
-tmux send-keys -t innovus_session 'exit' Enter
-wait_for_session_exit innovus_session
-
-tmux new-session -d -s innovus_session 'innovus -stylus'
-tmux send-keys -t innovus_session 'cd ../phy/db/part/Silago_bot_left_corner.enc.dat/' Enter
-tmux send-keys -t innovus_session 'source ../../../scr/pnr_partition.tcl' Enter
-tmux send-keys -t innovus_session 'exit' Enter
-wait_for_session_exit innovus_session
-
-tmux new-session -d -s innovus_session 'innovus -stylus'
-tmux send-keys -t innovus_session 'cd ../phy/db/part/Silago_top.enc.dat/' Enter
-tmux send-keys -t innovus_session 'source ../../../scr/pnr_partition.tcl' Enter
-tmux send-keys -t innovus_session 'exit' Enter
-wait_for_session_exit innovus_session
-
-tmux new-session -d -s innovus_session 'innovus -stylus'
-tmux send-keys -t innovus_session 'cd ../phy/db/part/Silago_bot.enc.dat/' Enter
-tmux send-keys -t innovus_session 'source ../../../scr/pnr_partition.tcl' Enter
-tmux send-keys -t innovus_session 'exit' Enter
-wait_for_session_exit innovus_session
+# tmux new-session -d -s innovus_session 'innovus -stylus'
+# tmux send-keys -t innovus_session 'cd ../phy/db/part/Silago_top_left_corner.enc.dat/' Enter
+# tmux send-keys -t innovus_session 'source ../../../scr/pnr_partition.tcl' Enter
+# tmux send-keys -t innovus_session 'exit' Enter
+# wait_for_session_exit innovus_session
+# 
+# tmux new-session -d -s innovus_session 'innovus -stylus'
+# tmux send-keys -t innovus_session 'cd ../phy/db/part/Silago_bot_left_corner.enc.dat/' Enter
+# tmux send-keys -t innovus_session 'source ../../../scr/pnr_partition.tcl' Enter
+# tmux send-keys -t innovus_session 'exit' Enter
+# wait_for_session_exit innovus_session
+# 
+# tmux new-session -d -s innovus_session 'innovus -stylus'
+# tmux send-keys -t innovus_session 'cd ../phy/db/part/Silago_top.enc.dat/' Enter
+# tmux send-keys -t innovus_session 'source ../../../scr/pnr_partition.tcl' Enter
+# tmux send-keys -t innovus_session 'exit' Enter
+# wait_for_session_exit innovus_session
+# 
+# tmux new-session -d -s innovus_session 'innovus -stylus'
+# tmux send-keys -t innovus_session 'cd ../phy/db/part/Silago_bot.enc.dat/' Enter
+# tmux send-keys -t innovus_session 'source ../../../scr/pnr_partition.tcl' Enter
+# tmux send-keys -t innovus_session 'exit' Enter
+# wait_for_session_exit innovus_session
 
 for i in  {1..5}; do
 	tmux new-session -d -s innovus_session_top 'innovus -stylus'
